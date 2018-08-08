d2d48d0af971b67df0dc19a4beb89f0899ddfd31
yingyu <635913935@qq.com>
2018-08-08 17:30:53 +0800

    ok
    完成

--- data.js
+++ data.js
@@ -8,6 +8,28 @@ var data = {
 			{
 				title:'益生菌肠胃宝',
 				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
+				price:'68',
+				onsale:'49',
+				imgurl:'./images/img1/img1.png'
+			},
+			{
+				title:'益生菌肠胃宝1',
+				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
+				price:'168',
+				onsale:'149',
+				imgurl:'./images/img1/img1.png'
+			}
+		]
+	},
+	//粮食
+	2:{
+		title:'哪吒队长-爱宠粮食',
+		txt:'宠之粮食系列-',
+		tip:'单罐包邮',
+		lists:[
+			{
+				title:'鸭脖子',
+				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
 				price:'68.00',
 				onsale:'49.00',
 				imgurl:'./images/img1/img1.png'
