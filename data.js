9109e54119dfadd830f36d5892a65c6dac2633a5
yingyu <635913935@qq.com>
2018-08-08 15:36:17 +0800

    add
    add files

---
+++ data.js
@@ -0,0 +1,17 @@
+var data = {
+	//零食 
+	1:{
+		title:'哪吒队长-爱宠零食',
+		txt:'宠之音系列-',
+		tip:'单罐包邮',
+		lists:[
+			{
+				title:'益生菌肠胃宝',
+				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
+				price:'68.00',
+				onsale:'49.00',
+				imgurl:'./images/img1/img1.png'
+			}
+		]
+	}
+}
