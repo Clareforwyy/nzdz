f043ecf72fe0ac07ce86210f5c88ddf970c3830d
yingyu <635913935@qq.com>
2018-08-09 17:40:38 +0800

    update

--- data.js
+++ data.js
@@ -21,7 +21,7 @@ var data = {
 			}
 		]
 	},
-	//粮食
+	//营养
 	2:{
 		title:'哪吒队长-爱宠粮食',
 		txt:'宠之粮食系列-',
@@ -30,57 +30,57 @@ var data = {
 			{
 				title:'鸭脖子',
 				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
-				price:'68.00',
-				onsale:'49.00',
+				price:'68',
+				onsale:'49',
 				imgurl:'./images/img1/img1.png'
 			},
 			{
 				title:'鸭脖子',
 				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
-				price:'68.00',
-				onsale:'49.00',
+				price:'68',
+				onsale:'49',
 				imgurl:'./images/img1/img1.png'
 			},
 			{
 				title:'鸭脖子',
 				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
-				price:'68.00',
-				onsale:'49.00',
+				price:'68',
+				onsale:'49',
 				imgurl:'./images/img1/img1.png'
 			},
 			{
 				title:'鸭脖子',
 				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
-				price:'68.00',
-				onsale:'49.00',
+				price:'68',
+				onsale:'49',
 				imgurl:'./images/img1/img1.png'
 			},
 			{
 				title:'鸭脖子',
 				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
-				price:'68.00',
-				onsale:'49.00',
+				price:'68',
+				onsale:'49',
 				imgurl:'./images/img1/img1.png'
 			},
 			{
 				title:'鸭脖子',
 				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
-				price:'68.00',
-				onsale:'49.00',
+				price:'68',
+				onsale:'49',
 				imgurl:'./images/img1/img1.png'
 			},
 			{
 				title:'鸭脖子',
 				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
-				price:'68.00',
-				onsale:'49.00',
+				price:'68',
+				onsale:'49',
 				imgurl:'./images/img1/img1.png'
 			},
 			{
 				title:'鸭脖子',
 				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
-				price:'68.00',
-				onsale:'49.00',
+				price:'68',
+				onsale:'49',
 				imgurl:'./images/img1/img1.png'
 			}
 		]
@@ -107,7 +107,7 @@ var data = {
 			}
 		]
 	},
-	//营养
+	//狗粮
 	4:{
 		title:'哪吒队长-爱宠营养',
 		txt:'宠之音系列-',
@@ -132,22 +132,113 @@ var data = {
 	//驱虫
 	5:{
 		title:'哪吒队长-爱宠驱虫',
-		txt:'宠之音系列-',
-		tip:'单罐包邮',
+		txt:'',
+		tip:'全场满88包邮 不满补运费6元 偏远另议',
 		lists:[
 			{
-				title:'益生菌肠胃宝',
-				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
-				price:'68',
-				onsale:'49',
-				imgurl:'./images/img1/img1.png'
+				title:'德国拜耳驱虫药一粒',
+				trait:'特点：1盒6粒 65元 用于治疗宠物犬的线虫和绦虫感染，如犬弓首蛔虫、 犬狮蛔虫线虫等体重6斤以下的狗狗 不要选择此药，容易有不良反应。吃法私聊卖家',
+				price:'',
+				onsale:'18',
+				imgurl:'./images/img5/1.png'
 			},
 			{
-				title:'益生菌肠胃宝1',
-				trait:'猫狗通用 调理宠物肠胃 缓解消化不良 （任两89包邮）',
-				price:'168',
-				onsale:'149',
-				imgurl:'./images/img1/img1.png'
+				title:'福来恩小型犬体外驱虫一支',
+				trait:'特点：一只装价格，每月一次， 一次一只，20斤以内用。 一盒（三支）129元',
+				price:'52',
+				onsale:'46',
+				imgurl:'./images/img5/2.png'
+			},
+			{
+				title:'福来恩中型犬体外驱虫一支',
+				trait:'特点：一只装价格，每月一次， 一次一只，20-40斤用。 一盒（三支）138元',
+				price:'55',
+				onsale:'50',
+				imgurl:'./images/img5/3.png'
+			},
+			{
+				title:'福来恩大型犬体外驱虫一支',
+				trait:'特点：一只装价格，每月一次， 一次一只，40-80斤用。 一盒（三支）148元',
+				price:'58',
+				onsale:'52',
+				imgurl:'./images/img5/4.png'
+			},
+			{
+				title:'犬心保小型犬体内驱虫一粒',
+				trait:'特点：每月一粒，体内驱虫必须做， 不要抱侥幸心理 一盒（六粒）109元',
+				price:'25',
+				onsale:'21',
+				imgurl:'./images/img5/5.png'
+			},
+			{
+				title:'犬心保中型犬体内驱虫一粒',
+				trait:'特点：每月一粒，为了家人和狗狗的 健康，请定期驱虫 一盒（六粒）138元',
+				price:'32',
+				onsale:'28',
+				imgurl:'./images/img5/6.png'
+			},
+			{
+				title:'犬心保大型犬体内驱虫一粒',
+				trait:'特点：每月一粒，体内驱虫必须做，40斤以上的狗狗 一盒（六粒）188元',
+				price:'39',
+				onsale:'35',
+				imgurl:'./images/img5/7.png'
+			},
+			{
+				title:'美国巴迪体内驱虫1粒',
+				trait:'特点：美国原装，1粒20斤狗用量， 自行切割，连吃两天 一盒（八粒）75元',
+				price:'15',
+				onsale:'10',
+				imgurl:'./images/img5/8.png'
+			},
+			{
+				title:'美国巴迪弓形虫球虫药1粒',
+				trait:'特点：专门针对弓形虫 球虫，可以和 普通体内驱虫配合使用吃法同上 一盒（八粒）75元',
+				price:'15',
+				onsale:'10',
+				imgurl:'./images/img5/9.png'
+			},
+			{
+				title:'大宠爱2.5kg一支',
+				trait:'特点：跳蚤 虱子 蛔虫 钩虫 体内外滴 的驱虫药 单支价，2.5kg以下使用 一盒（三支）129元',
+				price:'56',
+				onsale:'49',
+				imgurl:'./images/img5/10.png'
+			},
+			{
+				title:'大宠爱2.5-5kg一支',
+				trait:'特点：跳蚤 虱子 蛔虫 钩虫 体内外滴 的驱虫药 单支价，2.5-5kg使用 一盒（三支）188元',
+				price:'73',
+				onsale:'68',
+				imgurl:'./images/img5/11.png'
+			},
+			{
+				title:'大宠爱5-10kg一支',
+				trait:'特点：跳蚤 虱子 蛔虫 钩虫 体内外滴 的驱虫药 单支价，5-10kg使用 一盒（三支）208元',
+				price:'83',
+				onsale:'76',
+				imgurl:'./images/img5/12.png'
+			},
+			{
+				title:'大宠爱10-20kg一支',
+				trait:'特点：跳蚤 虱子 蛔虫 钩虫 体内外滴 的驱虫药 单支价，10-20kg使用 一盒（三支）328元 ',
+				price:'136',
+				onsale:'123',
+				imgurl:'./images/img5/13.png'
+			},
+			{
+				title:'大宠爱18-40kg一支',
+				trait:'特点：跳蚤 虱子 蛔虫 钩虫 体内外滴 的驱虫药 单支价，18-40kg使用 一盒（三支）379元',
+				price:'140',
+				onsale:'128',
+				imgurl:'./images/img5/14.png'
+			},
+			{
+				title:'虱骚清100ml',
+				trait:'特点：经常去草丛的要来一瓶这个经常喷 一喷',
+				price:'',
+				onsale:'20',
+				imgurl:'./images/img5/15.png'
 			}
 		]
 	},
